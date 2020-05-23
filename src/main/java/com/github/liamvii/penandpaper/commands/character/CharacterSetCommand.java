4745882542e8ac10533207d876bff961c4f93f79
Ren Binden <ren.binden@gmail.com>
2020-05-23 15:08:44 +0100

    Add a character set presence command, closes #16

--- src/main/java/com/github/liamvii/penandpaper/commands/character/CharacterSetCommand.java
+++ src/main/java/com/github/liamvii/penandpaper/commands/character/CharacterSetCommand.java
@@ -17,6 +17,7 @@ public final class CharacterSetCommand implements CommandExecutor {
     private final CharacterSetHeightCommand characterSetHeightCommand;
     private final CharacterSetWeightCommand characterSetWeightCommand;
     private final CharacterSetAppearanceCommand characterSetAppearanceCommand;
+    private final CharacterSetPresenceCommand characterSetPresenceCommand;
 
     public CharacterSetCommand(Pen plugin) {
         this.plugin = plugin;
@@ -24,6 +25,7 @@ public final class CharacterSetCommand implements CommandExecutor {
         this.characterSetHeightCommand = new CharacterSetHeightCommand(plugin);
         this.characterSetWeightCommand = new CharacterSetWeightCommand(plugin);
         this.characterSetAppearanceCommand = new CharacterSetAppearanceCommand(plugin);
+        this.characterSetPresenceCommand = new CharacterSetPresenceCommand(plugin);
     }
 
     @Override
@@ -58,11 +60,18 @@ public final class CharacterSetCommand implements CommandExecutor {
                             label,
                             Arrays.stream(args).skip(1).toArray(String[]::new)
                     );
+                case "presence":
+                    return characterSetPresenceCommand.onCommand(
+                            sender,
+                            command,
+                            label,
+                            Arrays.stream(args).skip(1).toArray(String[]::new)
+                    );
                 default:
-                    sender.sendMessage(RED + "Usage: /" + label + " set [age|height|weight|appearance]");
+                    sender.sendMessage(RED + "Usage: /" + label + " set [age|height|weight|appearance|presence]");
             }
         } else {
-            sender.sendMessage(RED + "Usage: /" + label + " set [age|height|weight|appearance]");
+            sender.sendMessage(RED + "Usage: /" + label + " set [age|height|weight|appearance|presence]");
         }
         return true;
     }
