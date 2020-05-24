798abd890ab3adeb8dc0b939c55ac6dd4726b5a1
LiamVII <44938368+LiamVII@users.noreply.github.com>
2020-05-24 15:45:29 +0100

    Added character set family name, closes #36

--- src/main/java/com/github/liamvii/penandpaper/commands/character/CharacterSetCommand.java
+++ src/main/java/com/github/liamvii/penandpaper/commands/character/CharacterSetCommand.java
@@ -83,11 +83,11 @@ public final class CharacterSetCommand implements CommandExecutor {
                             Arrays.stream(args).skip(1).toArray(String[]::new)
                     );
                 default:
-                    sender.sendMessage(RED + "Usage: /" + label + " set [firstname | age | height | weight | appearance | presence | exp ]");
+                    sender.sendMessage(RED + "Usage: /" + label + " set [firstname | familyname | age | height | weight | appearance | presence | exp ]");
                     break;
             }
         } else {
-            sender.sendMessage(RED + "Usage: /" + label + " set [firstname | age | height | weight | appearance | presence | exp ]");
+            sender.sendMessage(RED + "Usage: /" + label + " set [firstname | familyname | age | height | weight | appearance | presence | exp ]");
         }
         return true;
     }
