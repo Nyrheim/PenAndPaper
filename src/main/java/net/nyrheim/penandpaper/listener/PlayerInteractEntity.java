22b06d8b409a4346dff0155a87bbaf9cb9f12143
Ren Binden <ren.binden@gmail.com>
2020-06-14 17:38:42 +0100

    Validate hand on player interact, closes #113

--- src/main/java/net/nyrheim/penandpaper/listener/PlayerInteractEntity.java
+++ src/main/java/net/nyrheim/penandpaper/listener/PlayerInteractEntity.java
@@ -5,10 +5,13 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import static org.bukkit.inventory.EquipmentSlot.HAND;
+
 public class PlayerInteractEntity implements Listener {
 
     @EventHandler
     public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent event) {
+        if (event.getHand() != HAND) return;
         Player player = event.getPlayer();
         if (player.isSneaking() && event.getRightClicked() instanceof Player) {
             Player target = (Player) event.getRightClicked();
