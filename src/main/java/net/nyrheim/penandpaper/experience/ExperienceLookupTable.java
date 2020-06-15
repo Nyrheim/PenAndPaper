297a4acc19f2394f81c6b811c937993673f9dea8
Ren Binden <ren.binden@gmail.com>
2020-06-15 14:26:04 +0100

    Update experience lookup table, closes #116

--- src/main/java/net/nyrheim/penandpaper/experience/ExperienceLookupTable.java
+++ src/main/java/net/nyrheim/penandpaper/experience/ExperienceLookupTable.java
@@ -4,30 +4,20 @@ public final class ExperienceLookupTable {
 
     private ExperienceLookupTable() {}
 
-    public static final int MAX_EXPERIENCE = 355000;
+    public static final int MAX_EXPERIENCE = 45000;
 
     public static int getExperienceRequiredForLevel(int level) {
         switch (level) {
             case 1: return 0;
-            case 2: return 300;
-            case 3: return 900;
-            case 4: return 2700;
-            case 5: return 6500;
-            case 6: return 14000;
-            case 7: return 23000;
-            case 8: return 34000;
-            case 9: return 48000;
-            case 10: return 64000;
-            case 11: return 85000;
-            case 12: return 100000;
-            case 13: return 120000;
-            case 14: return 140000;
-            case 15: return 165000;
-            case 16: return 195000;
-            case 17: return 225000;
-            case 18: return 265000;
-            case 19: return 305000;
-            case 20: return 355000;
+            case 2: return 1000;
+            case 3: return 3000;
+            case 4: return 6000;
+            case 5: return 10000;
+            case 6: return 15000;
+            case 7: return 21000;
+            case 8: return 28000;
+            case 9: return 36000;
+            case 10: return 45000;
             default: return Integer.MAX_VALUE;
         }
     }
