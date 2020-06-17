ca62f8ee8721ab54844179bf34ff3b4ee1061bfa
LiamVII <44938368+LiamVII@users.noreply.github.com>
2020-06-17 17:19:21 +0100

    Updated playerlist and join/leave messages, closes #124

--- src/main/java/net/nyrheim/penandpaper/listener/PlayerListener.java
+++ src/main/java/net/nyrheim/penandpaper/listener/PlayerListener.java
@@ -16,34 +16,36 @@ public class PlayerListener implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        event.setJoinMessage("");
         player.setPlayerListHeader(ChatColor.GOLD + "<< << " + ChatColor.RED + "Welcome to Chronicles of Nyrheim " + ChatColor.GOLD + ">> >>" );
         player.setPlayerListFooter(ChatColor.RED + "Visit the website at: " + ChatColor.GOLD + "https://nyrheim.net/\n"
                 + ChatColor.GRAY + "Use " + ChatColor.WHITE + "/soul" + ChatColor.GRAY + " to set up your character.");
-        if (player.hasPermission("penandpaper.copper")) {
-            player.setPlayerListName(ChatColor.DARK_GREEN + player.getName());
-        }
-        if (player.hasPermission("penandpaper.silver")) {
-            player.setPlayerListName(ChatColor.GRAY + player.getName());
-        }
-        if (player.hasPermission("penandpaper.gold")) {
-            player.setPlayerListName(ChatColor.GOLD + player.getName());
-        }
-        if (player.hasPermission("penandpaper.platinum")) {
-            player.setPlayerListName(ChatColor.WHITE + "" + ChatColor.BOLD + player.getName());
-        }
-        else if (player.hasPermission("penandpaper.dungeonmaster")) {
-            player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName());
+        if (player.hasPermission("penandpaper.admin")) {
+            player.setPlayerListName(ChatColor.RED + player.getName());
         }
         else if (player.hasPermission("penandpaper.gamemaster")) {
             player.setPlayerListName(ChatColor.BLUE + player.getName());
         }
-        else if (player.hasPermission("penandpaper.admin")) {
-            player.setPlayerListName(ChatColor.RED + player.getName());
+        else if (player.hasPermission("penandpaper.dungeonmaster")) {
+            player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName());
+        }
+        else if (player.hasPermission("penandpaper.platinum")) {
+            player.setPlayerListName(ChatColor.DARK_AQUA + ""  + player.getName());
+        }
+        else if (player.hasPermission("penandpaper.gold")) {
+            player.setPlayerListName(ChatColor.GOLD + player.getName());
+        }
+        else if (player.hasPermission("penandpaper.silver")) {
+            player.setPlayerListName(ChatColor.GRAY + player.getName());
+        }
+        else if (player.hasPermission("penandpaper.copper")) {
+            player.setPlayerListName(ChatColor.DARK_GREEN + player.getName());
         }
 
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
+        event.setQuitMessage("");
     }
 }
