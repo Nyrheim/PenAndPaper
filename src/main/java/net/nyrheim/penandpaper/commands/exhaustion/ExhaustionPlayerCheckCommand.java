39332663d6992f48bbbf55906784c92345d9bec2
Ren Binden <ren.binden@gmail.com>
2020-06-29 02:17:54 +0100

    Add exhaustion change notifications, closes #137

--- src/main/java/net/nyrheim/penandpaper/commands/exhaustion/ExhaustionPlayerCheckCommand.java
+++ src/main/java/net/nyrheim/penandpaper/commands/exhaustion/ExhaustionPlayerCheckCommand.java
@@ -3,9 +3,9 @@ package net.nyrheim.penandpaper.commands.exhaustion;
 import net.nyrheim.penandpaper.PenAndPaper;
 import net.nyrheim.penandpaper.character.PenCharacter;
 import net.nyrheim.penandpaper.character.PenCharacterService;
+import net.nyrheim.penandpaper.exhaustion.ExhaustionTier;
 import net.nyrheim.penandpaper.player.PenPlayer;
 import net.nyrheim.penandpaper.player.PenPlayerService;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -34,25 +34,10 @@ public class ExhaustionPlayerCheckCommand implements CommandExecutor {
             return true;
         }
         int exhaustion = character.getExhaustion();
-        if (exhaustion <= 20) {
-            target.sendMessage(GREEN + "You feel energized.");
-        }
-        if (exhaustion > 20 && exhaustion <= 40) {
-            target.sendMessage(DARK_AQUA + "You feel fine.");
-        }
-        if (exhaustion > 40 && exhaustion <= 60) {
-            target.sendMessage(YELLOW + "You're starting to tire. Perhaps you should take a break.");
-        }
-        if (exhaustion > 60 && exhaustion <= 80) {
-            target.sendMessage(GRAY + "You're absolutely exhausted, you can't carry on for much longer.");
-        }
-        if (exhaustion > 80 && exhaustion < 100) {
-            target.sendMessage(RED + "You feel like you're about to collapse.");
-        }
-        if (exhaustion == 100) {
-            target.sendMessage(DARK_RED + "You are physically incapable of moving.");
-        }
+        ExhaustionTier tier = ExhaustionTier.forExhaustionValue(exhaustion);
+        if (tier == null) return true;
+        sender.sendMessage(tier.getMessageSelf());
         return true;
-        }
-
     }
+
+}
--- src/main/java/net/nyrheim/penandpaper/commands/exhaustion/ExhaustionRemoveCommand.java
+++ src/main/java/net/nyrheim/penandpaper/commands/exhaustion/ExhaustionRemoveCommand.java
@@ -3,6 +3,7 @@ package net.nyrheim.penandpaper.commands.exhaustion;
 import net.nyrheim.penandpaper.PenAndPaper;
 import net.nyrheim.penandpaper.character.PenCharacter;
 import net.nyrheim.penandpaper.character.PenCharacterService;
+import net.nyrheim.penandpaper.exhaustion.ExhaustionTier;
 import net.nyrheim.penandpaper.player.PenPlayer;
 import net.nyrheim.penandpaper.player.PenPlayerService;
 import org.bukkit.command.Command;
@@ -55,9 +56,19 @@ public final class ExhaustionRemoveCommand implements CommandExecutor {
             sender.sendMessage(RED + "That player does not currently have an active character.");
             return true;
         }
+        int oldExhaustion = character.getExhaustion();
         character.setExhaustion(character.getExhaustion() - amount);
         characterService.updateCharacter(character);
+        int newExhaustion = character.getExhaustion();
         sender.sendMessage(GREEN + "Exhaustion decreased.");
+        ExhaustionTier oldExhaustionTier = ExhaustionTier.forExhaustionValue(oldExhaustion);
+        ExhaustionTier newExhaustionTier = ExhaustionTier.forExhaustionValue(newExhaustion);
+        if (oldExhaustionTier != newExhaustionTier) {
+            target.sendMessage(newExhaustionTier.getMessageSelf());
+            if (sender != target) {
+                sender.sendMessage(newExhaustionTier.getMessageOther(character));
+            }
+        }
         return true;
     }
 }
